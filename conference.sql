f699644b5b4fd89d1c3ebf6cbba01254e8a20888
Riki <riki@Rikis-MacBook-Pro.local>
2019-02-05 15:23:40 -0500

    updated student table and moved professional and sponsor

--- conference.sql
+++ conference.sql
@@ -1,49 +1,6 @@
 CREATE DATABASE Conference;
 USE Conference;
 
-<<<<<<< HEAD
-CREATE TABLE CommitteeMember(
-=======
--- is there a way to enforce total participation or joint/disjoint properties?
-
-CREATE TABLE Person(
->>>>>>> added hotel room entity and roomassigned relation
-    id char(6) NOT NULL,
-    firstName varchar(20),
-    lastName varchar(30),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE Student(
-    id char(6) NOT NULL,
-    firstName varchar(20),
-    lastName varchar(30),
-    email varchar(50),
-<<<<<<< HEAD
-    PRIMARY KEY (id)
-=======
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Person(id) ON DELETE CASCADE
-);
--- ####################################################################
--- ############################STUDENTS################################
--- ####################################################################
-
-CREATE TABLE Student(
-    id char(6) NOT NULL,
-    roomNumber CHAR(3) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (roomNumber) REFERENCES HotelRoom (roomNumber) ON DELETE SET NULL,
-    FOREIGN KEY (id) REFERENCES Attendee(id) ON DELETE CASCADE
->>>>>>> added hotel room entity and roomassigned relation
-);
-
-CREATE TABLE HotelRoom(
-    rooomNumber char(3) NOT NULL,
-    numberOfBeds int,
-    PRIMARY KEY (roomNumber) 
-);
-
 
 CREATE TABLE Professional(
     id char(6) NOT NULL,
@@ -61,10 +18,33 @@ CREATE TABLE Sponsor(
     emailsSent int,
     PRIMARY KEY (id)
 );
--- ####################################################################
--- ############################MEMBERS#################################
--- ####################################################################
 
+CREATE TABLE CommitteeMember(
+    id char(6) NOT NULL,
+    firstName varchar(20),
+    lastName varchar(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Student(
+    id char(6) NOT NULL,
+    firstName varchar(20),
+    lastName varchar(30),
+    email varchar(50),
+    roomNumber char(3),
+    PRIMARY KEY (id),
+    FOREIGN KEY (roomNumber) REFERENCES HotelRoom(roomNumber) ON DELETE SET NULL
+);
+
+CREATE TABLE HotelRoom(
+    rooomNumber char(3) NOT NULL,
+    numberOfBeds int,
+    PRIMARY KEY (roomNumber) 
+);
+
+
+
+-- ############################MEMBERS#################################
 
 CREATE TABLE Subcommittee(
     subComitteeName varchar(30),
