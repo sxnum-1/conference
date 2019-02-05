cb5944e50c76b70b236d9daa99b3a91bb3272e7d
Riki <riki@Rikis-MacBook-Pro.local>
2019-02-05 15:43:23 -0500

    Final edits to student table

--- conference.sql
+++ conference.sql
@@ -8,13 +8,22 @@ CREATE TABLE CommitteeMember(
     PRIMARY KEY (id)
 );
 
+CREATE TABLE HotelRoom(
+    roomNumber char(3) NOT NULL,
+    numberOfBeds int,
+    PRIMARY KEY (roomNumber)
+);
+
 CREATE TABLE Student(
     id char(6) NOT NULL,
     firstName varchar(20),
     lastName varchar(30),
     email varchar(50),
-    PRIMARY KEY (id)
+    roomNumber char(3),
+    PRIMARY KEY (id),
+    FOREIGN KEY (roomNumber) REFERENCES HotelRoom (roomNumber) ON DELETE SET NULL
 );
+
 
 CREATE TABLE Professional(
     id char(6) NOT NULL,
@@ -43,6 +52,6 @@ CREATE TABLE isMember(
     subcommiteeName varchar(30) NOT NULL,
     isChair boolean,
     PRIMARY KEY (memberId, subcommiteeName),
-    FOREIGN KEY (memberId) REFERENCES CommitteeMember(id), # no cascading delete because of isChair property
-    FOREIGN KEY (subcommiteeName) REFERENCES Subcommittee(name)
+    FOREIGN KEY (memberId) REFERENCES CommitteeMember(id), -- no cascading delete because of isChair property
+    FOREIGN KEY (subcommiteeName) REFERENCES Subcommittee(subcommiteeName)
 );
