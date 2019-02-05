926545110ca4cabd63a0ee4dff45c572fb0aaa87
Riki <riki@Rikis-MacBook-Pro.local>
2019-02-05 14:54:42 -0500

    added hotel room entity and roomassigned relation

--- conference.sql
+++ conference.sql
@@ -1,7 +1,13 @@
 CREATE DATABASE Conference;
 USE Conference;
 
+<<<<<<< HEAD
 CREATE TABLE CommitteeMember(
+=======
+-- is there a way to enforce total participation or joint/disjoint properties?
+
+CREATE TABLE Person(
+>>>>>>> added hotel room entity and roomassigned relation
     id char(6) NOT NULL,
     firstName varchar(20),
     lastName varchar(30),
@@ -13,7 +19,34 @@ CREATE TABLE Student(
     firstName varchar(20),
     lastName varchar(30),
     email varchar(50),
+<<<<<<< HEAD
     PRIMARY KEY (id)
+=======
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES Person(id) ON DELETE CASCADE
+);
+-- ####################################################################
+-- ############################STUDENTS################################
+-- ####################################################################
+
+CREATE TABLE Student(
+    id char(6) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES Attendee(id) ON DELETE CASCADE
+>>>>>>> added hotel room entity and roomassigned relation
+);
+
+CREATE TABLE HotelRoom(
+    rooomNumber char(3) NOT NULL,
+    numberOfBeds int,
+    PRIMARY KEY (roomNumber) 
+);
+CREATE TABLE RoomAssigned(
+    id char(6) NOT NULL,
+    roomNumber CHAR(3) NOT NULL,
+    PRIMARY KEY (id, roomNumber),
+    FOREIGN KEY (id) REFERENCES  Student(id),
+    FOREIGN KEY (roomNumber) REFERENCES HotelRoom(roomNumber)
 );
 
 CREATE TABLE Professional(
@@ -32,10 +65,14 @@ CREATE TABLE Sponsor(
     emailsSent int,
     PRIMARY KEY (id)
 );
+-- ####################################################################
+-- ############################MEMBERS#################################
+-- ####################################################################
+
 
 CREATE TABLE Subcommittee(
-    name varchar(30),
-    PRIMARY KEY (name)
+    subComitteeName varchar(30),
+    PRIMARY KEY (subComitteeName)
 );
 
 CREATE TABLE isMember(
@@ -43,6 +80,9 @@ CREATE TABLE isMember(
     subcommiteeName varchar(30) NOT NULL,
     isChair boolean,
     PRIMARY KEY (memberId, subcommiteeName),
-    FOREIGN KEY (memberId) REFERENCES CommitteeMember(id), # no cascading delete because of isChair property
-    FOREIGN KEY (subcommiteeName) REFERENCES Subcommittee(name)
+    FOREIGN KEY (memberId) REFERENCES CommitteeMember(id), -- no cascading delete because of isChair property
+    FOREIGN KEY (subcommiteeName) REFERENCES Subcommittee(subComitteeName)
 );
+
+
+
