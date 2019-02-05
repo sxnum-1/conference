a8d935d01507590e38dc423ff6f903fefd20d51f
Kyusung Shim <15ks2@caslab.queensu.ca>
2019-02-05 13:59:04 -0500

    sql: change Person heritance structure

--- conference.sql
+++ conference.sql
@@ -1,50 +1,36 @@
 CREATE DATABASE Conference;
 USE Conference;
 
-# is there a way to enforce total participation or joint/disjoint properties?
-
-CREATE TABLE Person(
+CREATE TABLE CommitteeMember(
     id char(6) NOT NULL,
     firstName varchar(20),
     lastName varchar(30),
-    aptNum int,
-    street varchar(30),
-    zipCode char(6),
-    city varchar(30),
-    province varchar(30),
     PRIMARY KEY (id)
-);
-
-CREATE TABLE CommitteeMember(
-    id char(6) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Person(id) ON DELETE CASCADE
-);
-
-CREATE TABLE Attendee(
-    id char(6) NOT NULL,
-    email varchar(50),
-    PRIMARY KEY (id)
-    FOREIGN KEY (id) REFERENCES Person(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Student(
     id char(6) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Attendee(id) ON DELETE CASCADE
+    firstName varchar(20),
+    lastName varchar(30),
+    email varchar(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Professional(
     id char(6) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Attendee(id) ON DELETE CASCADE
+    firstName varchar(20),
+    lastName varchar(30),
+    email varchar(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Sponsor(
     id char(6) NOT NULL,
+    firstName varchar(20),
+    lastName varchar(30),
+    email varchar(50),
     emailsSent int,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Attendee(id) ON DELETE CASCADE
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Subcommittee(
