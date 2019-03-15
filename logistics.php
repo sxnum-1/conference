8ba4c29e63592e66dac85c53fae721d3b90c01c6
Kyusung Shim <15ks2@caslab.queensu.ca>
2019-03-15 19:43:46 -0400

    complete logistics.php

--- logistics.php
+++ logistics.php
@@ -1,34 +1,103 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <link href="conference.css" type="text/css" rel="stylesheet" />
-  </head>
-  <body>
+</head>
+
+<body>
     <nav>
-      <ul class="navbar">
-        <li><a href="index.php">Events</a></li>
-        <li><a href="attendees.php">Attendees</a></li>
-        <li><a href="sponsors.php">Sponsors</a></li>
-        <li><a href="jobs.php">Jobs</a></li>
-        <li><a href="hotel.php">Hotel</a></li>
-        <li><a href="committees.php">Committees</a></li>
-        <li><a class="active" href="logistics.php">Logistics</a></li>
-      </ul>
+        <ul class="navbar">
+            <li><a href="index.php">Events</a></li>
+            <li><a href="attendees.php">Attendees</a></li>
+            <li><a href="sponsors.php">Sponsors</a></li>
+            <li><a href="jobs.php">Jobs</a></li>
+            <li><a href="hotel.php">Hotel</a></li>
+            <li><a href="committees.php">Committees</a></li>
+            <li><a class="active" href="logistics.php">Logistics</a></li>
+        </ul>
     </nav>
     <header>
-      <!-- placeholder -->
-      <h1>stuff for the logistics</h1>
+        <h1>Logistics</h1>
     </header>
     <div class="main">
-      <h2>
-        show the total intake; break this down by total registration amounts and
-        total sponsorship amounts
-      </h2>
-      <!-- placeholder -->
+
+        <!-- set up pdo -->
+        <?php include "pdo.php" ?>
+
+        <div id="registrationintake">
+            <?php
+            $studentQuery = "SELECT count(id) AS studentCount FROM Student";
+            $studentStmt = $pdo->prepare($studentQuery);
+            $studentStmt->execute();
+
+            $studentAmount = (int)$studentStmt->fetch()["studentCount"] * 50;
+
+            $profQuery = "SELECT count(id) AS profCount FROM Professional";
+            $profStmt = $pdo->prepare($profQuery);
+            $profStmt->execute();
+
+            $profAmount = (int)$profStmt->fetch()["profCount"] * 100;
+
+            $attendeeTotal = $studentAmount + $profAmount;
+
+            echo "<div id=\"registrationintake\">";
+            echo "<h3>Registration intake</h3>";
+            echo "<table>";
+            echo "<tr><td>Student registration intake</td><td>\$$studentAmount</td></tr>";
+            echo "<tr><td>Professional registration intake</td><td>\$$profAmount</td></tr>";
+            echo "<tr><th>Total registration intake</th><th>\$$attendeeTotal</th></tr>";
+            echo "</table>";
+            echo "</div>";
+
+            $sponsorshipLevels = array("Bronze", "Silver", "Gold", "Platinum");
+            $sponsorshipTotal = 0;
+
+            echo "<div id=\"sponsorshipregistrationintake\">";
+            echo "<h3>Sponsorship intake</h3>";
+            echo "</div>";
+            echo "<table>";
+            foreach ($sponsorshipLevels as $level) {
+              $sponsorshipTotal += createRowEntry($level, $pdo);
+            }
+            echo "<tr><th>Total sponsorship intake</th><th>\$$sponsorshipTotal</th></tr>";
+            echo "</table>";
+
+            $total = $attendeeTotal + $sponsorshipTotal;
+            echo "<h2>Total intake: \$$total</h2>";
+
+            function createRowEntry($grade, $pdo)
+            {
+              switch ($grade) {
+                case "Bronze":
+                  $multiplier = 1000;
+                  break;
+                case "Silver":
+                  $multiplier = 3000;
+                  break;
+                case "Gold":
+                  $multiplier = 5000;
+                  break;
+                default: // Platinum
+                  $multiplier = 10000;
+              }
+
+              $query = "SELECT count(companyName) AS cnt FROM SponsorCompany GROUP BY ranking HAVING ranking=\"$grade\"";
+              $stmt = $pdo->prepare($query);
+              $stmt->execute();
+
+              $amount = (int)$stmt->fetch()["cnt"] * $multiplier;
+
+              echo "<tr><td>$grade sponsor intake</td><td>\$$amount</td></tr>";
+              return $amount;
+            }
+            ?>
+        </div>
     </div>
     <footer>
-      
+
     </footer>
 
-  </body>
-</html>
+</body>
+
+</html> 
