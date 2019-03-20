51cb402aff8066f4291eb954f5aa5fde328ac9a4
Riki <riki@Rikis-MacBook-Pro.local>
2019-03-20 10:06:25 -0400

    changed the php script setEvent

--- setEvent.php
+++ setEvent.php
@@ -1,29 +1,38 @@
 <?php
     // Allows the use of a single pdo variable throughout all scripts
     include './pdo.php';
+    //Variables passed by the user in a post method from the form created in getSelects.php
     $sname = $_POST["sessionname"];
     $day = $_POST["sessionday"];
     $time = $_POST["sessiontime"];
     $room = $_POST["sessionroom"];
     // Queries the database looking for events specific to the event selected
-    $query = "SELECT sessionName, UNIX_TIMESTAMP(startTime) as startTime, room FROM SessionEvent WHERE sessionName='$sname';";
+    $query = "SELECT * FROM SessionEvent WHERE sessionName='$sname' AND room='$room' AND startTime=FROM_UNIXTIME($time);";
     $stmt = $pdo->prepare($query);
     $stmt->execute();
-    // Creates a table with the information. The information that is echoed is sent to fetch API.
-    while ($event = $stmt->fetch()) {
-        if ($room == $event["room"] && $time == $event["startTime"]){
-            echo "<p>Cannot include duplicates. Please try again</p>";
-            echo "<button><a href='http://192.168.64.3/conference/attendees.php'>Back</a></button>";
-        }
-        else{
-            if ($day == "saturday"){
-                //updates the session event to a new time.
-                $query = "UPDATE SessionEvent SET startTime=FROM_UNIXTIME($time) endTime=MAKEDATE($endTime) room=$room WHERE sessionName=$sname";
-            }
-            else{
 
-            }
+    //If not in the database
+    if ($stmt->rowCount() <= 0){
+        if ($day == "saturday"){
+            //updates the session event to a new time. Sessions are assumed to be an hour long.
+            $query = "UPDATE SessionEvent SET startTime=FROM_UNIXTIME($time), endTime=MAKEDATE($time+3600), room=$room WHERE sessionName=$sname";
         }
+        else{//sunday
+            $time = $time + 86400;//this constant represents 24hrs in seconds
+            $query = "UPDATE SessionEvent SET startTime=FROM_UNIXTIME($time), endTime=MAKEDATE($time+3600), room='$room' WHERE sessionName='$sname'";
+        }
+        echo $query;
+        //sends query to database
+        // $stmt = $pdo->prepare($query);
+        // if(!$stmt->execute()) {
+        //     echo "<p>Something went wrong. Could not update entry";
+        // }
+        echo "<p>Session Event is updated</p>";
     }
+    // If already in the database then prints a back button to event page and informs user. 
+    else{
+        echo "<p>Cannot include duplicates. Please try again</p>";
+    }
+    echo "<button><a href='http://192.168.64.3/conference/index.php'>Back</a></button>";
 ?>
 
