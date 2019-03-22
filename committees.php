f23969c8868bd1ba6c1cdace8a894cbdd94eae9d
BWS <tanguo@hotmail.com>
2019-03-22 11:54:35 -0400

    Add files via upload

--- committees.php
+++ committees.php
@@ -18,12 +18,47 @@
     <header>
       <h1>stuff for committees</h1>
 
-    </header>
+      </header>
     <div class="main">
-      <!-- placeholder -->
-      <h2>list all members of a subcommittee (drop-down menu)</h2>
-      <!-- placeholder -->
-
+       <!-- set up PDO -->
+       <?php include 'pdo.php'; ?>
+      <!--Company and job related information -->
+      <div id="committeepage">
+        <h2>list members of subcommittee (dropdown)</h2>
+        <div id="roomselection">
+          <p>Select a subcommittee you would like to view:</p>
+          
+          <?php
+            // Select Room number from hotel rooms
+            $query = 'SELECT subcommitteename FROM Subcommittee;';
+            $stmt = $pdo->prepare($query);
+            $stmt->execute();
+            echo "<select name=\"subcommitteename\" onChange=\"\">";
+            echo "<option value=''></option>";
+            //Constructs the options looping from the query call. 
+            while ($name = $stmt->fetch()){
+              echo "<option value=" . $name['subcommitteename'] . ">" . $name["subcommitteename"] . "</option>";
+            }
+            echo "</select><br>";
+          ?>
+        </div>
+        <div id="display"></div>
+      </div>
+      <?php
+        //Lists the students name and id
+        $query = 'SELECT CONCAT(firstName,\', \',lastName) as FName FROM isMember JOIN CommitteeMember ON CommitteeMember.id = isMember.memberId;';
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        //creates a table
+        echo "<table>";
+        echo "<tr><th>Name</th></tr>";
+        // Loops through and displays each row.
+        while ($name = $stmt->fetch()) {
+          $name = $name["FName"];
+          echo "<tr><td>$name</td></tr>";
+        }
+        echo "</table>";
+      ?>
     </div>
     <footer>
 
--- hotel.php
+++ hotel.php
@@ -20,11 +20,76 @@
       <h1>Stuff for the hotel</h1>
     </header>
     <div class="main">
-      <h2>list all students for a room</h2>
+       <!-- set up PDO -->
+       <?php include 'pdo.php'; ?>
+      <!--Company and job related information -->
+      <div id="studentrooms">
+        <h2>list all students in a room</h2>
+        <div id="roomselection">
+          <p>Select a Room Number you would like to check:</p>
+          
+          <?php
+            // Select Room number from hotel rooms
+            $query = 'SELECT roomNumber FROM hotelroom;';
+            $stmt = $pdo->prepare($query);
+            $stmt->execute();
+            echo "<select name=\"roomNumber\" onChange=\"displayRoom(this)\">";
+            echo "<option value=''></option>";
+            //Constructs the options looping from the query call. 
+            while ($room = $stmt->fetch()){
+              echo "<option value=" . $room['roomNumber'] . ">" . $room["roomNumber"] . "</option>";
+            }
+            echo "</select><br>";
+          ?>
+        </div>
+        <div id="display"></div>
+      </div>
+      <?php
+        //Lists the students name and id
+        $query = 'SELECT CONCAT(firstName,\', \',lastName) as FName FROM student;';
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        //creates a table
+        echo "<table>";
+        echo "<tr><th>Name</th></tr>";
+        // Loops through and displays each row.
+        while ($name = $stmt->fetch()) {
+          $name = $name["FName"];
+          echo "<tr><td>$name</td></tr>";
+        }
+        echo "</table>";
+      ?>
     </div>
     <footer>
       
     </footer>
     <!-- placeholder -->
-  </body>
+
+  <script>
+    // Function used to display job specific rolls taking in the selected company as a parameter
+    function displayRoom(room){
+      //Target div to display info
+      let content = document.getElementById("display");
+      let a = room.value;
+      const query = `
+        <?php
+          // $query = "SELECT roomNumber FROM HotelRoom";
+          $room = '';
+          $room = isset($_POST['roomNumber']) ? $_POST['roomNumber'] : '';
+          $query = "SELECT student.firstName, student.lastName, roomNumber FROM student WHERE student.roomNumber = '$room' GROUP BY student.roomNumber";
+          $stmt = $pdo->prepare($query);
+          $stmt->execute();
+
+          while ($room = $stmt->fetch()) {
+              echo "<option>$room</option>";
+          }
+
+          echo "</select><br>";
+          ?>
+        `;
+      content.innerHTML = "<h3>" + "Student Name" + "</h3>" + query;
+    }
+  </script>
+</body>
+
 </html>
